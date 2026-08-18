5ab0226c80387724dc2ab76b5130ab67cb94bd4e
TosaTimeOfficial <tosacollects@gmail.com>
2026-08-18 11:08:24 -0700

    Staff Bio Updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  base: process.env.GITHUB_PAGES === "true" ? "/k9-global-rescue-website/" : "/",
   output: "static",
+  vite: {
+    server: {
+      allowedHosts:
+        process.env.ALLOWED_HOSTS === "all"
+          ? true
+          : (process.env.ALLOWED_HOSTS?.split(",") ?? [])
+    }
+  },
   build: {
     format: "directory"
   }
